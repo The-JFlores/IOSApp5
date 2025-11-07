d35cbd0df4238e2f471deaab16d9f9dcd14f6c71
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-11-07 09:34:27 -0500

    Add NoteEditView.swift

---
+++ IOSApp5/Views/NoteEditView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteEditView.swift
+//  IOSApp5
+//
+//  Created by Jose Flores on 2025-11-07.
+//
+
+import SwiftUI
+
+struct NoteEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteEditView()
+}
