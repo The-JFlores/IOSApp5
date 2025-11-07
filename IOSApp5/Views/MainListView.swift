79208ffd54ef2c0e9375ecb4d29fb42adf469137
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-11-07 09:33:05 -0500

    Add MainListView.swift

---
+++ IOSApp5/Views/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  IOSApp5
+//
+//  Created by Jose Flores on 2025-11-07.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
