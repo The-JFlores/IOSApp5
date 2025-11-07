590cded315e370b2fea1b53d119e3949822776ea
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-11-07 09:25:50 -0500

    created the views folder and added the Rootview file

---
+++ IOSApp5/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  IOSApp5
+//
+//  Created by Jose Flores on 2025-11-07.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
