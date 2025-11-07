80be221b02162044a0c0be4536be1c29dcff6865
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-11-07 09:35:54 -0500

    Add NoteDetailView.swift

---
+++ IOSApp5/Views/NoteDetailView.swift.swift
@@ -0,0 +1,18 @@
+//
+//  NoteDetailView.swift.swift
+//  IOSApp5
+//
+//  Created by Jose Flores on 2025-11-07.
+//
+
+import SwiftUI
+
+struct NoteDetailView_swift: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteDetailView_swift()
+}
